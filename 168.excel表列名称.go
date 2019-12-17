26efb39beb8bf383e51eb315640419381ef3b4c4
bingjian <939496716@qq.com>
2019-12-17 20:57:39 +0800

    upload

--- 168.excel表列名称.go
+++ 168.excel表列名称.go
@@ -6,7 +6,18 @@
 
 // @lc code=start
 func convertToTitle(n int) string {
-    
+	var res bytes.Buffer
+	arr:=[]string{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+	temp:=[]string{}
+	for n > 0{
+			n--
+			temp = append(temp,arr[n%26])
+			n = n/26
+	}
+	for i:=len(temp)-1;i>=0;i--{
+		res.WriteString(temp[i])
+	}
+	return res.String()
 }
 // @lc code=end
 
