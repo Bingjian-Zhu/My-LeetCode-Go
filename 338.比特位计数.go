443861dd66498432d8e88e9ae4f36e0cce923780
Bingjian-Zhu <939496716@qq.com>
2020-01-05 22:53:13 +0800

    DP专题

---
+++ 338.比特位计数.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=338 lang=golang
+ *
+ * [338] 比特位计数
+ */
+
+// @lc code=start
+func countBits(num int) []int {
+	res := make([]int, num + 1)
+    res[0] = 0
+    for i := 1; i <= num; i++{
+		if(i % 2 == 1){
+			res[i] = res[i-1] + 1
+		}else{
+			res[i] = res[i/2]
+		}
+	}
+	return res
+}
+// @lc code=end
+
