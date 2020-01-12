aba07d0e6975abc344954665e757ee4c7516634a
bingjian <939496716@qq.com>
2020-01-12 20:57:40 +0800

    up

---
+++ 344.反转字符串.go
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=golang
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+func reverseString(s []byte)  {
+	i,j:=0,len(s)-1
+	for i<j{
+		s[i],s[j] = s[j],s[i]
+		i++
+		j--
+	}
+}
+// @lc code=end
+
